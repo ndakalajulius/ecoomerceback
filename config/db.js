1f1ec8ea762cddfa3927bf75f4c6c911bf7e044e
ndakalajulius <juliusndakala98@gmail.com>
2024-11-06 17:44:57 +0300

    updated .env file

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    // Remove the deprecated options
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (error) {
-    console.error(error.message);
+    console.error('Error connecting to MongoDB:', error.message);
     process.exit(1);
   }
 };
+
 module.exports = connectDB;
